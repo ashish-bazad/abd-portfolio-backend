cfe8516eb4aa78584ff563d762a34fa89eb319cb
ashish-bazad <ashish.student.cse22@iitbhu.ac.in>
2024-06-12 19:57:08 +0530

    deployment requirement update

--- backend/deployment.py
+++ backend/deployment.py
@@ -4,7 +4,6 @@ from .settings import BASE_DIR
 
 SECRET_KEY = os.environ('SECRET')
 ALLOWED_HOSTS = [os.environ('WEBSITE_HOSTNAME')]
-print(ALLOWED_HOSTS)
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ('WEBSITE_HOSTNAME')]
 DEBUG = False
 
