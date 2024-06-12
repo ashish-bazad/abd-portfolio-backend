61b34eaaa912eb5446493ee8e15278b6d5a58bd8
ashish-bazad <ashish.student.cse22@iitbhu.ac.in>
2024-06-12 20:48:54 +0530

    azure

--- backend/deployment.py
+++ backend/deployment.py
@@ -2,9 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ('SECRET')
-ALLOWED_HOSTS = [os.environ('WEBSITE_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ('WEBSITE_HOSTNAME')]
+SECRET_KEY = os.environ.get('SECRET')
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME')]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -22,8 +22,8 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ('AZURE_POSTGRESQL_CONNECTIONSTRING')
-parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
+parameters = dict(item.split('=') for item in connection_string.split(' '))
 
 DATABASES = {
     'default': {
