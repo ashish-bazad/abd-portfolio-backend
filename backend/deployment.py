13cc0f28533d2b7a3824d0e874caa303fa89cd00
ashish-bazad <ashish.student.cse22@iitbhu.ac.in>
2024-06-12 22:07:02 +0530

    azure hosting

--- backend/deployment.py
+++ backend/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ.get('SECRET')
 ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME'), 'http://' + os.environ.get('WEBSITE_HOSTNAME')]
-DEBUG = True
+DEBUG = False
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
